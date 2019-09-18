6937cf10fa61356bdea667b2c3ce884374e9b39e
purvimisal <purvivmisal@gmail.com>
2019-09-17 18:46:36 -0700

    problems

---
+++ lc-88.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 14:58:51 2019
+
+@author: purvi
+"""
+
+def merge(nums1, m, nums2, n): 
+        count = 0
+        n1 = [i for i in nums1 if i!= 0]
+        for i in nums1: 
+            if(i ==0): count+=1
+        if (m>= n):
+            nums1[:] = n1 +nums2
+            nums1.sort()
+        elif(count>=n):
+            nums1[:] = nums2 
+
+if __name__ == "__main__":
+    nums1 = [0]
+    m = 0
+    nums2 = [1]  
+    n = 1
+    
+    merge(nums1, m, nums2, n)
+    print(nums1)
